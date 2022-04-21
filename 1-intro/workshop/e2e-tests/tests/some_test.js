84705c26eb84232b2ba11b4b4d1919084faced60
Assim mht <assimmht235@gmail.com>
2022-04-21 13:46:44 +0200

    end 2 end test

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,7 @@
-Feature('Ynov Nantes');
+Feature("Ynov Nantes");
 
-Scenario('Test Ynov Nantes Land Page', ({ I }) => {
-    I.amOnPage('https://www.ynov-nantes.com/');
+Scenario("Test Ynov Nantes Land Page", ({ I }) => {
+  I.amOnPage("https://www.ynov-nantes.com/");
+  I.click(".icon-search");
+  I.fillField("s", "Info");
 });
