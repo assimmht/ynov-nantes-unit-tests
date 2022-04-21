3bb81d580ce23d805e563559083ba223608cde5d
Assim mht <assimmht235@gmail.com>
2022-04-21 16:06:19 +0200

    unit tests

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -4,8 +4,39 @@ use PHPUnit\Framework\TestCase;
 
 require_once('./src/TempTracker.php');
 
-class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEqual(42, 42);
+class TempTrackerTest extends TestCase
+{
+
+    public function test_something()
+    {
+        $this->assertEquals(42, 42);
+    }
+
+    public function test_get_max()
+    {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_max(), 55);
+    }
+
+    public function test_get_min()
+    {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_min(), 45);
+    }
+
+
+    public function test_get_mean()
+    {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_mean(), 50.0);
     }
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -8,6 +8,23 @@ class FlattenTest extends TestCase
 {
     public function test_something()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEquals(1337, 1337);
+    }
+
+    public function test_single_value()
+    {
+        $this->assertEquals(flatten([5]), [5]);
+        $this->assertEquals(flatten([-5]), [-5]);
+    }
+
+    public function test_few_value()
+    {
+        $this->assertEquals(flatten([1, 2, 3]), [1, 2, 3]);
+    }
+
+    public function test_nested_once()
+    {
+        $this->assertEquals(flatten([5, [1]]), [5, 1]);
+        $this->assertEquals(flatten([5, [1, [2]]]), [5, 1, 2]);
     }
 }
